c69beeab8b4064abefcacc46a0e4822fcdbf2bd5
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-10-28 01:33:05 -0500

    automatically searched and requested query

--- captcha/testingWebDriver.py
+++ captcha/testingWebDriver.py
@@ -1,23 +1,29 @@
+import webbrowser
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# Ensure geckodriver is installed and set in the PATH
-GeckoDriverManager().install()
+def google_search_and_click(queries):
+    base_url = "https://www.google.com/search?q={}"
 
-# Initialize the Firefox driver without specifying the executable_path
-browser = webdriver.Firefox()
+    # Ensure geckodriver is installed and set in the PATH
+    GeckoDriverManager().install()
 
-# Navigate to Google
-browser.get("https://www.google.com")
+    # Initialize the Firefox driver
+    browser = webdriver.Firefox()
 
-# Find the search box, type a query, and submit
-search_box = browser.find_element('q')
-search_box.send_keys('Hello, Selenium!')
-search_box.submit()
+    for query in queries:
+        # Navigate to the search query using Selenium (instead of webbrowser)
+        browser.get(base_url.format(query))
 
-# Wait for a bit to see the results
-import time
-time.sleep(3)
+        # Wait for the result to be clickable and then click it
+        xpath_to_select = '/html/body/div[5]/div/div[12]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div/span/a'
+        wait = WebDriverWait(browser, 10)
+        element = wait.until(EC.element_to_be_clickable((By.XPATH, xpath_to_select)))
+        element.click()
 
-# Close the browser
-browser.quit()
+if __name__ == "__main__":
+    topics = ["oracle 1z0-808 question {} exam topics".format(i) for i in range(1, 4)]
+    google_search_and_click(topics)
