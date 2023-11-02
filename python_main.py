d89c546f79937f2ab7468501a127fcc4a8e7bc27
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-11-01 21:42:49 -0500

    added modular code

---
+++ python_main.py
@@ -0,0 +1,154 @@
+# amazon
+
+# ... [rest of the code]
+
+if __name__ == "__main__":
+    topics = ["AWS Certified Cloud Practitioner (CLF-C01) question {} examtopics".format(i) for i in range(1, 451)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["AWS Certified Cloud Practitioner CLF-C02 question {} examtopics".format(i) for i in range(1, 142)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Amazon AWS Certified Developer Associate question {} examtopics".format(i) for i in range(1, 451)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Amazon AWS Certified Cloud Practitioner CLF-C02 question {} examtopics".format(i) for i in range(1, 142)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Amazon AWS Certified DevOps Engineer - Professional DOP-C02 question {} examtopics".format(i) for i in range(1, 145)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Amazon AWS Certified Solutions Architect - Associate SAA-C03 question {} examtopics".format(i) for i in range(1, 631)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Amazon AWS Certified Solutions Architect - Professional question {} examtopics".format(i) for i in range(1, 1028)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Amazon AWS Certified SysOps Administrator - Associate question {} examtopics".format(i) for i in range(1, 385)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Amazon AWS DevOps Engineer Professional question {} examtopics".format(i) for i in range(1, 214)]
+    google_search_and_save_links(topics)
+
+
+
+
+
+
+# google
+
+if __name__ == "__main__":
+    topics = ["Google Associate Cloud Engineer question {} examtopics".format(i) for i in range(1, 256)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Google Professional Cloud Architect question {} examtopics".format(i) for i in range(1, 280)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Google Professional Cloud DevOps Engineer question {} examtopics".format(i) for i in range(1, 176)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Google Professional Cloud Network Engineer question {} examtopics".format(i) for i in range(1, 160)]
+    google_search_and_save_links(topics)
+
+
+
+
+# java
+
+
+
+if __name__ == "__main__":
+    topics = ["Oracle 1z0-808 question {} examtopics".format(i) for i in range(1, 101)]  # Assuming a default range
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Oracle 1z0-809 question {} examtopics".format(i) for i in range(1, 101)]  # Assuming a default range
+    google_search_and_save_links(topics)
+
+
+if __name__ == "__main__":
+    topics = ["Oracle 1z0-819 question {} examtopics".format(i) for i in range(1, 182)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Oracle 1z0-082 question {} examtopics".format(i) for i in range(1, 164)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Oracle 1z0-083 question {} examtopics".format(i) for i in range(1, 187)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Oracle 1z0-1072-22 question {} examtopics".format(i) for i in range(1, 60)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Oracle 1z0-1072-23 question {} examtopics".format(i) for i in range(1, 65)]
+    google_search_and_save_links(topics)
+
+
+
+
+
+# microsoft
+
+
+# ... [rest of the code]
+
+if __name__ == "__main__":
+    topics = ["Microsoft AI-102 question {} examtopics".format(i) for i in range(1, 240)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Microsoft AI-900 question {} examtopics".format(i) for i in range(1, 251)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Microsoft AZ-400 question {} examtopics".format(i) for i in range(1, 473)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Microsoft AZ-500 question {} examtopics".format(i) for i in range(1, 460)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Microsoft AZ-900 question {} examtopics".format(i) for i in range(1, 452)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Microsoft PL-400 question {} examtopics".format(i) for i in range(1, 233)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Microsoft PL-500 question {} examtopics".format(i) for i in range(1, 105)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Microsoft SC-100 question {} examtopics".format(i) for i in range(1, 195)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Microsoft SC-200 question {} examtopics".format(i) for i in range(1, 236)]
+    google_search_and_save_links(topics)
+
+if __name__ == "__main__":
+    topics = ["Microsoft SC-900 question {} examtopics".format(i) for i in range(1, 193)]
+    google_search_and_save_links(topics)
+
+
+
+
+
+
+
