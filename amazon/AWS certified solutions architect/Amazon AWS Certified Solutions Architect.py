e6397a941450a4024fccd6a2914f97854ab0d71e
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-11-02 22:53:02 -0500

    commiting progress

--- amazon/AWS certified solutions architect/Amazon AWS Certified Solutions Architect.py
+++ amazon/AWS certified solutions architect/Amazon AWS Certified Solutions Architect.py
@@ -54,5 +54,5 @@ def google_search_and_save_links(queries):
     browser.quit()
 
 if __name__ == "__main__":
-    topics = ["Amazon AWS Certified Solutions Architect - Professional question {} examtopics".format(i) for i in range(1, 1028)]
+    topics = ["Amazon AWS Certified Solutions Architect - Professional question {} examtopics".format(i) for i in range(30, 1028)]
     google_search_and_save_links(topics)
