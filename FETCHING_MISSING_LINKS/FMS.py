3f778fe7976b3b890c99e9ba48e8b93e691a6ba1
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-11-10 12:39:39 -0600

    added function to fetch missing links

--- FETCHING_MISSING_LINKS/FMS.py
+++ FETCHING_MISSING_LINKS/FMS.py
@@ -1,28 +1,41 @@
-import re
 
-# Load the links from the file
-with open('fileLinks.txt', 'r') as file:
+import re
+import os
+
+# Function to extract total links expected from the file name
+def extract_total_from_filename(filename):
+    match = re.search(r'../google/Google Associate Cloud Engineer question/links_1-(\d+)', filename)
+    return int(match.group(1)) if match else None
+
+# Load the links from the file and the total expected links
+filename = '../google/Google Associate Cloud Engineer question/links_1-255'
+total_expected_links = extract_total_from_filename(filename)
+
+with open(filename, 'r') as file:
     links = file.readlines()
 
 # Sort the list of links to ensure they are in order
-links = sorted(links)
+links = sorted(links, key=lambda x: int(re.search(r"question-(\d+)", x).group(1)))
 
 # Regular expression to match the question numbers in the links
-pattern = re.compile(r"question-(\d+)-discussion")
+pattern = re.compile(r"question-(\d+)")
 
 # Find missing question numbers
 missing_numbers = []
+last_number = 0  # Will hold the last number found to find missing ones up to total_expected_links
 for i in range(len(links) - 1):
     current_match = pattern.search(links[i])
     next_match = pattern.search(links[i + 1])
     if current_match and next_match:
-        # Extract question numbers from current and next link
         current_number = int(current_match.group(1))
         next_number = int(next_match.group(1))
-        # Check if the next question number is not the consecutive number
         if next_number != current_number + 1:
-            # Add the range of missing numbers to the list
             missing_numbers.extend(range(current_number + 1, next_number))
+    last_number = int(current_match.group(1))
+
+# Add the remaining missing numbers if the last link's number is less than total_expected_links
+if total_expected_links and last_number < total_expected_links:
+    missing_numbers.extend(range(last_number + 1, total_expected_links + 1))
 
 # Print out missing question numbers
 print("Missing question numbers:")
