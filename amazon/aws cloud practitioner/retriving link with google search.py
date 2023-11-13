ea1ac18e287e5f9e2f8877bf2ac274dca4dc315b
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-11-12 22:35:00 -0600

    adding progress

--- amazon/aws cloud practitioner/retriving link with google search.py
+++ amazon/aws cloud practitioner/retriving link with google search.py
@@ -47,3 +47,6 @@ def google_search_and_save_links(queries):
 if __name__ == "__main__":
     topics = ["aws cloud practitioner question {} examtopics".format(i) for i in range(147, 199)]
     google_search_and_save_links(topics)
+
+
+
