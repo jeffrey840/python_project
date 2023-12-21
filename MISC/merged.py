ada0839de3462d013c04623144b4f8963b301173
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-12-20 22:20:16 -0600

    deleted some txt files

---
+++ MISC/merged.py
@@ -0,0 +1,16 @@
+# Define the range of file numbers
+file_numbers = range(1, 16)  # This will create a sequence from 1 to 15
+
+# Open the merged file in write mode
+with open('merged-1_15.txt', 'w') as merged_file:
+    # Iterate through each file number
+    for num in file_numbers:
+        # Construct the file name
+        file_name = f'text_file_{num}.txt'
+
+        # Open the individual file in read mode
+        with open(file_name, 'r') as individual_file:
+            # Read the content and write it to the merged file
+            merged_file.write(individual_file.read())
+            # Optionally, add a newline between files if needed
+            merged_file.write('\n')
