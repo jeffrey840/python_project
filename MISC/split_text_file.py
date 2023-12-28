27e0b1b906b2173f62bd1e02abe3e4d417a5841b
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-12-27 21:43:29 -0600

    added ch7 txt files

--- MISC/split_text_file.py
+++ MISC/split_text_file.py
@@ -4,7 +4,7 @@ def split_text_file(file_path, max_word_count):
         text = file.read()
 
     # Text to be added at the beginning of each file
-    additional_text = """given teh following text provide several interesting topic to write a paper about, For instance, if you picked the criminal justice system in Texas, you can pick the death penalty in Texas and discuss the pros and cons of it (again using at least 2 outside resources not including your text).\n\n"""
+    additional_text = """given the following text provide only important facts that were mentioned in the text. Also return a small keyword in the text so that i can find it later \n\n"""
 
     # Count total words
     words = text.split()
@@ -27,7 +27,5 @@ def split_text_file(file_path, max_word_count):
         file_counter += 1
 
 # Usage
-split_text_file('govt_ch3.txt', 1400)  # Replace 'path_to_your_file.txt' with your file path and 900 with your desired word limit per file
+split_text_file('Unit_2/govt_ch7.txt', 900)  # Replace 'path_to_your_file.txt' with your file path and 900 with your desired word limit per file
 
-
-# For this assignment, you will need to read chapter 3 in your text. What I am looking for in this assignment is for you to become the instructor for chapter 3 and do a power point presentation that would help others learn the important facts about chapter 3 (as well as why this is important). You need to have at least 10 slides (i.e- ten important facts) and a final slide that summarizes the chapter overall.
