fddfd5a554ea813b808ccb1febba48d5236240ec
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-01-16 02:39:50 -0600

    updated function for zillow fetch

--- District_Map/zillow_listing_forRent.py
+++ District_Map/zillow_listing_forRent.py
@@ -1,3 +1,6 @@
+
+
+
 import http.client
 import json
 import os
@@ -9,7 +12,7 @@ def fetch_zillow_listings_http(zipcode, page=1):
         'X-RapidAPI-Key': "206a0782demsh5a1d5a242226a6dp1f6b6cjsnf08b18ac4dfa",
         'X-RapidAPI-Host': "zillow-com1.p.rapidapi.com"
     }
-    path = f"/propertyExtendedSearch?location={zipcode}&page={page}&status_type=ForSale"
+    path = f"/propertyExtendedSearch?location={zipcode}&page={page}&status_type=ForRent"
 
     conn.request("GET", path, headers=headers)
     res = conn.getresponse()
@@ -28,18 +31,17 @@ def save_to_json(data, filename):
     with open(full_path, 'w') as file:
         json.dump(data, file, indent=4)
 
+
 def main():
-    zipcodes = ["77088", "77037"]  # Add more zip codes as needed
-    combined_data = []
+    zipcodes = ["77091", "77096", "77080"]  # Add more zip codes as needed
     for zipcode in zipcodes:
         page = 1
         while True:
             data = fetch_zillow_listings_http(zipcode, page)
             if "error" not in data:
-                # Change 'listings' to 'props' here
-                combined_data.extend(data['props'])
-                print(f"Data for {zipcode} (Page {page}) added to combined data.")
-
+                filename = f"zillow_listings_{zipcode}_page{page}_forRent.json"
+                save_to_json(data, filename)
+                print(f"Data for {zipcode} (Page {page}) saved successfully.")
                 # Check if there are more pages to fetch
                 if page < data['totalPages']:
                     page += 1
@@ -49,13 +51,7 @@ def main():
             else:
                 print(f"Error: {data['error']} Status Code: {data['status_code']} for {zipcode} (Page {page})")
                 break
-    filename = "combined_2_zillow_listings.json"
-    save_to_json(combined_data, filename)
-    print(f"Combined data saved successfully to {filename}.")
 
 if __name__ == "__main__":
     main()
 
-
-# district 1 = 91 & 96
-# district 2 88 & 37
