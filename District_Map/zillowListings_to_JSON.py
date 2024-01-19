ad7fe4e0c20a372c32ab1d193f25c9aeff24cd5a
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-01-18 21:58:29 -0600

    fetechd complete district 1 and 2

--- District_Map/zillowListings_to_JSON.py
+++ District_Map/zillowListings_to_JSON.py
@@ -6,7 +6,7 @@ import time
 def fetch_zillow_listings_http(zipcode, page=1):
     conn = http.client.HTTPSConnection("zillow-com1.p.rapidapi.com")
     headers = {
-        'X-RapidAPI-Key': "206a0782demsh5a1d5a242226a6dp1f6b6cjsnf08b18ac4dfa",
+        'X-RapidAPI-Key': "ff711f390emsh45c0fec2c0bc47fp18ac1djsn35accd7f9e03",
         'X-RapidAPI-Host': "zillow-com1.p.rapidapi.com"
     }
     path = f"/propertyExtendedSearch?location={zipcode}&page={page}&status_type=ForSale"
@@ -29,7 +29,8 @@ def save_to_json(data, filename):
         json.dump(data, file, indent=4)
 
 def main():
-    zipcodes = ["77088", "77037"]  # Add more zip codes as needed
+    zipcodes = ["77088", "77037", "77088", "77037", "77016", "77050", "77091", "77076", "77093", "77078", "77028", "77026", "77022", "77018", "77092", "77009", "77026", "77020", "77013", "77029"]
+
     combined_data = []
     for zipcode in zipcodes:
         page = 1
